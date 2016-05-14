bc1328dfa38d658cb14da7f44a305db0059d5a3c
ks-s-a <s.ksenofontov@sup.com>
2016-05-14 12:20:36 +0300

    add comments and minor fixes

--- feya.js
+++ feya.js
@@ -1,16 +1,18 @@
-var http = require('http'),
-    faye = require('faye'),
-    express = require('express'),
-    app = express();
+// Пример работы с модулем faye
+var express = require('express');
+var faye = require('faye');
+var http = require('http');
+    
+var app = express();
+var server = http.Server(app);
+var bayeux = new faye.NodeAdapter({mount: '/f'});
 
-var server = http.Server(app),
-    bayeux = new faye.NodeAdapter({mount: '/f'});
-
+// Раздаем статику
 app.use('/', express.static(__dirname + '/static'));
 
+// Регистрируем на сервере
 bayeux.attach(server);
 
-var PORT = 8000;
-server.listen(PORT, function () {
-  console.log('App was launched on '+ PORT +' port');
+server.listen(8000, function () {
+  console.log('App was launched on 8000 port');
 });
--- sockets.js
+++ sockets.js
@@ -1,18 +1,22 @@
+// Пример использования Websockets
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var PORT = 8000;
 
+// Раздаем статику
 app.use('/', express.static(__dirname + '/static'));
 
+// Обработка сообщений Websockets
 io.sockets.on('connection', function (socket) {
-  console.dir(socket, {depth: null});
+  console.log(socket); // Посмотрим объект
 
+  // Событие закрытия соединения
   socket.on('disconnect', function () {
     console.log('user disconnected');
   })
 
+  // Событие прихода соощения от клиента
   socket.on('message', function (message) {
     console.log('message is: ', message);
 
@@ -24,6 +28,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-server.listen(PORT, function () {
-  console.log('Our app was launched on the '+ PORT + ' port.');
+server.listen(8000, function () {
+  console.log('Our app was launched on the 8000 port.');
 });
